a0f2677fe891e68169eaa5daa7180659e6a814e8
Sungho <hurgj123kr@gmail.com>
2024-07-25 21:28:42 +0900

    fav api add. and authcontext update

---
+++ src/api/fav.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:4000/api/fav";
+
+// 로그인한 유저의 포켓몬 즐겨찾기 데이터를 가져옴.
+export const fetchFav = async (token: string) => {
+  const response = await axios.get(`${API_URL}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data.fav;
+};
+
+// 유저가 추가한 포켓몬을 데이터에 추가.
+export const addFav = async ({
+  token,
+  pokemonName,
+}: {
+  token: string;
+  pokemonName: string;
+}) => {
+  const response = await axios.post(
+    `${API_URL}/add`,
+    { pokemonName },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  return response.data;
+};
+
+// 유저의 즐겨찾기한 포켓몬 삭제
+export const removeFav = async ({
+  token,
+  pokemonName,
+}: {
+  token: string;
+  pokemonName: string;
+}) => {
+  const response = await axios.delete(`${API_URL}/remove/${pokemonName}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -5,6 +5,7 @@ import React, {
   ReactNode,
   useEffect,
 } from "react";
+import { fetchFav } from "../api/fav";
 
 // type
 interface AuthContextType {
@@ -12,14 +13,19 @@ interface AuthContextType {
   setToken: (token: string | null) => void;
   login: (token: string) => Promise<void>;
   logout: (showNotification?: (message: string) => void) => void;
+  fav: string[];
+  setFav: (fav: string[]) => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
+// authProvider는 로그인, 로그아웃시 알림을 알려주는것
 const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [token, setToken] = useState<string | null>(() => {
     return localStorage.getItem("token");
   });
+
+  const [fav, setFav] = useState<string[]>([]);
 
   useEffect(() => {
     if (token) {
@@ -34,10 +40,17 @@ const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
 
   const login = async (token: string) => {
     setToken(token);
+    try {
+      const fetchedFav = await fetchFav(token);
+      setFav(fetchedFav);
+    } catch (err) {
+      console.error("Failed to fetch fav on login", err);
+    }
   };
 
   const logout = (showNotification?: (meesage: string) => void) => {
     setToken(null);
+    setFav([]);
     if (showNotification) {
       showNotification("Logout successful!");
     }
@@ -50,7 +63,23 @@ const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
         logout();
       }
     };
+
+    // fav불러오기
+    const loadFav = async () => {
+      if (token) {
+        try {
+          const fetchedFav = await fetchFav(token);
+          setFav(fetchedFav);
+        } catch (err) {
+          console.error("Failed to fetch fav", err);
+        }
+      } else {
+        setFav([]);
+      }
+    };
+
     checkToken();
+    loadFav();
 
     const intervalId = setInterval(checkToken, 60 * 1000);
 
@@ -58,7 +87,9 @@ const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   }, [token]);
 
   return (
-    <AuthContext.Provider value={{ token, setToken, login, logout }}>
+    <AuthContext.Provider
+      value={{ token, setToken, login, logout, fav, setFav }}
+    >
       {children}
     </AuthContext.Provider>
   );
